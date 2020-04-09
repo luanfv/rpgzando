8fd4733b644d7da76a4c179007c042a48844bd2e
Luan <luanfrancavieira@gmail.com>
2020-04-09 12:59:13 -0300

    picker of selected race

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,7 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
+// @react-native-community/picker
+import com.reactnativecommunity.picker.RNCPickerPackage;
 
 public class PackageList {
   private Application application;
@@ -56,7 +57,8 @@ public class PackageList {
 
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
-      new MainReactPackage(mConfig)
+      new MainReactPackage(mConfig),
+      new RNCPickerPackage()
     ));
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,64 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, Button} from 'react-native'
 import {handleRace} from './helpers/rules'
+import {Picker} from '@react-native-community/picker'
 
 export default () => 
 {
-    console.log(handleRace(1, 1))
-    console.log(handleRace(2))
-    console.log(handleRace(3, 2))
-    console.log(handleRace(4, 2))
-    console.log(handleRace(5, 1))
-    console.log(handleRace(6, 7))
-    console.log(handleRace(1, 3))
+    const races = [
+        {
+            id: 1, 
+            name: 'hill dwarf', 
+            race: 1, 
+            subRace: 1, 
+            desc: 'this race gives you +2 constitution for being a dwarf and + 1 wisdom for being from the hill'
+        },
+        {
+            id: 2, 
+            name: 'mountain dwarf', 
+            race: 1, 
+            subRace: 2, 
+            desc: 'this race gives you +2 constitution for being a dwarf and +2 strength for being from the hill'
+        },
+    ]
+    const [raceId, setRaceId] = useState(races[0].id)
+    const [race, setRace] = useState({
+        id: races[0].id, 
+        name: races[0].name, 
+        race: races[0].race, 
+        subRace: races[0].subRace, 
+        desc: races[0].desc
+    })
+    
+    const handleRaceSelected = (key) =>
+    {
+        races.map(e => {
+            if(e.id === key)
+            {
+                setRace(e)
+                setRaceId(e.id)
+
+                return
+            }
+        })
+    }
+
     return (
         <View>
-            <Text>Hey, it's my app of RPG!</Text>
+            <Text>Select your race</Text>
+            <Picker
+                selectedValue={raceId}
+                onValueChange={value => handleRaceSelected(value)}
+            >
+                {races.map(value => <Picker.Item key={value.id} value={value.id} label={value.name} />)}
+            </Picker>
+
+            <View key={race.id}>
+                <Text>{race.name}</Text>
+                <Text>{race.desc}</Text>
+            </View>
+
+            <Button onPress={() => console.log(handleRace(race.race, race.subRace))} title="Confirm" />
         </View>
     )
 }

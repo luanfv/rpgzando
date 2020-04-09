08847f9d7aae10cfb6a1a0cbec19cafa96c46175
Luan <luanfrancavieira@gmail.com>
2020-04-08 21:18:17 -0300

    start project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default () => 
+{
+    return (
+        <View>
+            <Text>Hey, it's my app of RPG!</Text>
+        </View>
+    )
+}

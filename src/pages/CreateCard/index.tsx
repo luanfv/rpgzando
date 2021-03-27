e72225506dc17af61259f83eb7116f49cb9e762e
Luan <luanfrancavieira@gmail.com>
2021-03-27 11:53:16 -0300

    select

--- src/pages/CreateCard/index.tsx
+++ src/pages/CreateCard/index.tsx
@@ -1,21 +1,106 @@
-import React from 'react';
+import React, { useRef, useState, useCallback } from 'react';
+import { Form } from '@unform/mobile';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FormHandles } from '@unform/core';
 
+import { races, professions } from '../../utils/rules';
 import Content from '../../components/Content';
 import ProgressBar from '../../components/ProgressBar';
 import Input from '../../components/Input';
 
-import { Main, Container } from './style';
+import {
+  Main,
+  Container,
+  Title,
+  SelectImageContainer,
+  SelectImage,
+  SelectContainer,
+  Select,
+  SelectText,
+} from './style';
 
 const CreateCard: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const [race, setRace] = useState(races[0]);
+  const [profession, setProfession] = useState(professions[0]);
+
+  const handleRaceSelected = useCallback((key) => {
+    const raceSelected = races.find((_race) => _race.id === key);
+
+    if (raceSelected) {
+      setRace(raceSelected);
+    }
+  }, []);
+
+  const handleProfessionSelected = useCallback((key) => {
+    const findProfession = professions.find(
+      (_profession) => _profession.id === key,
+    );
+
+    if (findProfession) {
+      setProfession(findProfession);
+    }
+  }, []);
+
   return (
     <Content title="Nova Ficha" goBack>
       <Main>
         <ProgressBar phase={1} />
 
-        <Container>
-          <Input title="Nome" />
-          <Input title="Nível" keyboardType="numeric" />
-        </Container>
+        <Form ref={formRef} onSubmit={() => console.log('submit')}>
+          <Container>
+            <Title>Informações do base</Title>
+            <Container>
+              <Input title="Nome" />
+              <Input title="Nível" keyboardType="numeric" />
+            </Container>
+
+            <Container>
+              <SelectImageContainer>
+                <SelectImage source={race.image} />
+              </SelectImageContainer>
+
+              <SelectContainer>
+                <Select
+                  selectedValue={Number(race.id)}
+                  onValueChange={(value) => handleRaceSelected(value)}
+                >
+                  {races.map((value) => (
+                    <Select.Item
+                      key={String(value.id)}
+                      value={Number(value.id)}
+                      label={String(value.name)}
+                    />
+                  ))}
+                </Select>
+              </SelectContainer>
+              <SelectText>{race.desc}</SelectText>
+            </Container>
+
+            <Container>
+              <SelectImageContainer>
+                <SelectImage source={profession.image} />
+              </SelectImageContainer>
+
+              <SelectContainer>
+                <Select
+                  selectedValue={Number(profession.id)}
+                  onValueChange={(value) => handleProfessionSelected(value)}
+                >
+                  {professions.map((value) => (
+                    <Select.Item
+                      key={String(value.id)}
+                      value={Number(value.id)}
+                      label={String(value.name)}
+                    />
+                  ))}
+                </Select>
+              </SelectContainer>
+              <SelectText>HP: 1d{profession.hp} (por nível)</SelectText>
+            </Container>
+          </Container>
+        </Form>
       </Main>
     </Content>
   );
--- src/pages/CreateCard/style.ts
+++ src/pages/CreateCard/style.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components/native';
+import { Picker } from '@react-native-community/picker';
+
+import styles from '../../styles.json';
 
 export const Main = styled.View`
   margin: 20px;
@@ -6,4 +9,44 @@ export const Main = styled.View`
 
 export const Container = styled.View`
   margin: 20px 0;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+export const SelectImageContainer = styled.View`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background-color: ${styles.primary};
+  margin: auto;
+  overflow: hidden;
+`;
+
+export const SelectImage = styled.Image`
+  width: 75px;
+  height: 75px;
+  margin: auto;
+`;
+
+export const SelectContainer = styled.View`
+  border-radius: 4px;
+  overflow: hidden;
+  margin: 10px 0;
+`;
+
+export const Select = styled(Picker)`
+  color: ${styles.bg};
+  font-size: 18px;
+  background-color: rgba(255, 255, 255, 0.4);
+  padding: 8px 12px;
+`;
+
+export const SelectText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
 `;

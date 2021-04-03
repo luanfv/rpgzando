47b23958ff0dea88d50cd0c993687d973bdc77b6
Luan <luanfrancavieira@gmail.com>
2021-04-03 18:16:29 -0300

    benefits of the attributes

--- src/pages/Cards/Create/Attributes/index.tsx
+++ src/pages/Cards/Create/Attributes/index.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useMemo, useState, useEffect } from 'react';
 
 import { useCards } from '../../../../hooks/CardsContext';
 
-import { professions } from '../../../../utils/rules';
+import { professions, handleRace, IAttributes } from '../../../../utils/rules';
 
 import Content from '../../../../components/Content';
 import ProgressBar from '../../../../components/ProgressBar';
@@ -11,7 +12,7 @@ import InputNumeric from '../../../../components/InputNumeric';
 import { Main, Title, Container } from '../style';
 
 const Attributes: React.FC = () => {
-  const { level, profession } = useCards();
+  const { level, profession, race } = useCards();
 
   const [hp, setHp] = useState(level as Number);
   const [force, setForce] = useState(1 as Number);
@@ -20,6 +21,7 @@ const Attributes: React.FC = () => {
   const [charisma, setCharisma] = useState(1 as Number);
   const [wisdom, setWisdom] = useState(1 as Number);
   const [intelligence, setIntelligence] = useState(1 as Number);
+  const [benefits, setBenefits] = useState({} as IAttributes);
 
   const fullHp = useMemo(() => {
     const found = professions.find(
@@ -29,6 +31,14 @@ const Attributes: React.FC = () => {
     return Number(found?.hp);
   }, [profession.id]);
   const maxHp = useMemo(() => Number(level) * Number(fullHp), [fullHp, level]);
+
+  useEffect(() => {
+    const attributes = handleRace(race.id, race.idSecondary);
+
+    if (attributes) {
+      setBenefits(attributes);
+    }
+  }, [race.id, race.idSecondary]);
 
   return (
     <Content title="Nova Ficha" goBack>
@@ -50,7 +60,7 @@ const Attributes: React.FC = () => {
 
         <Container>
           <InputNumeric
-            title={'Força'}
+            title={`Força (+${benefits.for})`}
             value={Number(force)}
             onChange={setForce}
             min={1}
@@ -58,7 +68,7 @@ const Attributes: React.FC = () => {
             random
           />
           <InputNumeric
-            title={'Destreza'}
+            title={`Destreza (+${benefits.dex})`}
             value={Number(dexterity)}
             onChange={setDexterity}
             min={1}
@@ -66,7 +76,7 @@ const Attributes: React.FC = () => {
             random
           />
           <InputNumeric
-            title={'Constituição'}
+            title={`Constituição (+${benefits.con})`}
             value={Number(constitution)}
             onChange={setConstitution}
             min={1}
@@ -74,7 +84,7 @@ const Attributes: React.FC = () => {
             random
           />
           <InputNumeric
-            title={'Inteligência'}
+            title={`Inteligência (+${benefits.int})`}
             value={Number(intelligence)}
             onChange={setIntelligence}
             min={1}
@@ -82,7 +92,7 @@ const Attributes: React.FC = () => {
             random
           />
           <InputNumeric
-            title={'Sabedoria'}
+            title={`Sabedoria (+${benefits.wis})`}
             value={Number(wisdom)}
             onChange={setWisdom}
             min={1}
@@ -90,7 +100,7 @@ const Attributes: React.FC = () => {
             random
           />
           <InputNumeric
-            title={'Carisma'}
+            title={`Carisma (+${benefits.cha})`}
             value={Number(charisma)}
             onChange={setCharisma}
             min={1}

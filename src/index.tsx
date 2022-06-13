f59a199a024e188c303e88220bb9ec193b559d62
luanfv <luanfrancavieira@gmail.com>
2022-06-13 13:52:33 -0300

    feat: add yup

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { View, Button, TextInput } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 import { Input } from '@src/components';
+
+interface IForm {
+  name: string;
+  level: string;
+}
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Você precisa informar o nome!'),
+  level: Yup.number()
+    .required('Você precisa informar o nível!')
+    .typeError('Esta entrada precisa ser do tipo númerica!')
+    .integer('Você precisa passar um número inteiro!'),
+});
 
 const App: React.FC = () => {
   const {
@@ -15,12 +30,15 @@ const App: React.FC = () => {
       name: '',
       level: '',
     },
+    resolver: yupResolver(schema),
   });
 
   const nameRef = useRef<TextInput>(null);
   const levelRef = useRef<TextInput>(null);
 
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = useCallback((data: IForm) => {
+    console.log(data);
+  }, []);
 
   return (
     <View>
@@ -36,7 +54,7 @@ const App: React.FC = () => {
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
-            errorMessage={errors.name && 'Você precisa informar o nome!'}
+            errorMessage={errors.name && errors.name.message}
             onSubmitEditing={() => levelRef.current?.focus()}
           />
         )}
@@ -57,7 +75,8 @@ const App: React.FC = () => {
             value={value}
             keyboardType="decimal-pad"
             autoCorrect={false}
-            errorMessage={errors.level && 'Você precisa informar o nível!'}
+            maxLength={3}
+            errorMessage={errors.level && errors.level.message}
           />
         )}
         name="level"

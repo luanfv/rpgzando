073222f2710555f934f39aea3659879b210f431d
luanfv <luanfrancavieira@gmail.com>
2022-06-20 14:48:46 -0300

    refactor: file name type

--- src/types/components/index.d.ts
+++ src/types/components/index.d.ts
@@ -1,4 +1,4 @@
-export type { IPicker, IPickerItem } from './picker';
-export type { IInput } from './input';
-export type { IInputNumeric } from './inputNumeric';
-export type { ICheckboxList, ISkillList } from './checkboxList';
+export type { IPicker, IPickerItem } from './Picker';
+export type { IInput } from './Input';
+export type { IInputNumeric } from './InputNumeric';
+export type { ICheckboxList, ISkillList } from './CheckboxList';
